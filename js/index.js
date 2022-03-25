2bb2a935be41cdcc3e4a3d3f62a919d56103e8c6
Carrie Miskin <clove40@gmail.com>
2022-03-25 18:09:43 -0400

    added users and photos

--- js/index.js
+++ js/index.js
@@ -10,12 +10,25 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function getUsers(username) {
-    fetch(` https://api.github.com/search/users?q=${username}`,{
+    fetch(` https://api.github.com/search/users?q=${username}`, {
         method: "GET",
         headers: {
             Accept: 'application/vnd.github.v3+json'
         }
     })
     .then(response => response.json())
-    .then(response => console.log('response', response))
+    .then(response => response.items.map(item => displayUser(item)))
+}
+
+
+function displayUser(user) {
+    const userList = document.querySelector("#user_list")
+    const li = document.createElement("li")
+    const image = document.createElement("img")
+    image.src = user.avatar_url
+    image.alt = user.login
+    const h3 = document.createElement("h3")
+    h3.textContent = user.login
+    li.append(image, h3)
+    userList.append(li)
 }
